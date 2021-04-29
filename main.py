e4f2c011fdcd0379ccfb83125337c18f5e7e59ce
Federico Nery <fnery@cys.com.ar>
2021-04-29 16:33:06 -0300

    se corrige git ignore y se agrega main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def pluralize(lista):
+    setDePalabras = set()
+    #codigo...
+    return setDePalabras
+
+def same_length(secuencia):
+    #codigo...
+    return False
+
+if __name__ == '__main__':
+    print(pluralize(["cow", "pig", "cow", "cow"]))
+    print(same_length("110011100010"))
